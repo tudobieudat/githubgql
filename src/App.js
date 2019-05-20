9d1f486993b29fdbe5f62b57549480a5b10d7f12
Tu Do <tudobieudat@protonmail.com>
2019-05-19 22:47:47 -0700

    Added axios and changed the App to class component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>RAUXA CODING CHALLENGE</h1>
-    </div>
-  );
+const access_token = process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN;
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>RAUXA CODING CHALLENGE</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
